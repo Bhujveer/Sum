f3a7d09bbc74eabdad0439a6a0c76c8be1f1b525
Bhujveer Singh <bhujveer375@gmail.com>
2021-12-03 17:00:59 +0530

    Add project files.

---
+++ Sum/Test.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sum
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                int b=S(10, 11);
+                Response.Write("The sum of Two Numbers is="+b);
+            }
+
+        }
+
+        public int S(int a,int b)
+        {
+            int s;
+            s = a + b;
+            return s;
+
+        }
+    }
+}
